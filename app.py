4c82abccad0d87f6c5bf21470f76032478900253
Gottipati Anusha <gottipatianusha134@gmail.com>
2026-03-11 23:12:37 +0530

    Initial commit - Linear Regression DevOps project

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-x=np.array([1,2,3,4,5]).reshape(-1,1)
-y=np.array([3,5,7,9,11])
-model=LinearRegression()
+
+x = np.array([1,2,3,4,5]).reshape(-1,1)
+y = np.array([3,5,7,9,11])
+
+model = LinearRegression()
 model.fit(x,y)
-prediction=model.predict([[6]])
-print("predicted salary for 6 years experience:",prediction[0])
-print("KMEC deploying product")
+
+prediction = model.predict([[6]])
+
+print("Predicted salary for 6 years experience:", prediction[0])
